76414d52e600ae344817436f99125643f1d99809
JGAntunes <j.goncalo.antunes@gmail.com>
2018-05-17 01:25:02 +0100

    fix: config rebase issue

---
+++ src/core/runtime/config-nodejs.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = () => ({
+  Addresses: {
+    Swarm: [
+      '/ip4/0.0.0.0/tcp/4002',
+      '/ip4/127.0.0.1/tcp/4003/ws'
+    ],
+    API: '/ip4/127.0.0.1/tcp/5002',
+    Gateway: '/ip4/127.0.0.1/tcp/9090'
+  },
+  Discovery: {
+    MDNS: {
+      Enabled: false,
+      Interval: 10
+    },
+    webRTCStar: {
+      Enabled: true
+    }
+  },
+  Bootstrap: []
+})
